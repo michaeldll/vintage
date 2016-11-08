c626284e95cd46de296c7d4923fafa1c504ac86d
Seyort <mathieu.gilles10@gmail.com>
2016-11-08 11:15:35 +0100

    Add files via upload

---
+++ pages/flotte.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="Vintage Wings"/>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://www.vintagewings.com" />
+    <meta property="og:image" content="http://www.vintagewings.com/images/og.jpg" />
+    <meta name="description" content="Reservez un voyage dans un avion légéndaire!">
+    <meta name="author" content="Vintage Wings">
+    <meta name="keywords" content="Vintage, Wings, Vol, Reservation">
+    <link rel="icon" type="image/ico" href="http://195.83.128.55/~mmid116a05/img/icone1.ico">
+    <title>Vintage Wings</title>
+    <script src="https://use.fontawesome.com/ffa9c99182.js"></script>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/baseStyle.css" rel="stylesheet">
+    <link href="../zoombox/zoombox.css" rel="stylesheet" type="text/css" media="screen" />
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+    <?php include("navbar.php"); ?>
+
+    <!-- description flotte -->
+    <div class="centered"><h1>Notre flotte</h1></div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam leo, tincidunt sed magna id, efficitur pulvinar leo. Donec neque mauris, placerat ac congue ac, scelerisque ac elit. Nullam dignissim metus elit, quis rhoncus libero molestie vel. Etiam aliquet, arcu quis tempor venenatis, ligula risus blandit mauris, aliquam scelerisque nisl arcu id lectus.
+            </p>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <!-- sous-menu avions -->
+    <div class="centered">
+        <div class="btn-group" role="group">
+            <a href="#avion1" type="button" class="btn btn-default js-scrollTo">Avion 1</a>
+            <a href="#avion2" type="button" class="btn btn-default js-scrollTo">Avion 2</a>
+            <a href="#avion3" type="button" class="btn btn-default js-scrollTo">Avion 3</a>
+            <a href="#avion4" type="button" class="btn btn-default js-scrollTo">Avion 4</a>
+            <a href="#avion5" type="button" class="btn btn-default js-scrollTo">Avion 5</a>
+        </div>
+    </div>
+
+    <!-- description avion 1 -->
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <h3 id="avion1">Avion 1</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit cursus euismod. Quisque at pellentesque leo. Vestibulum vitae aliquam neque. Vestibulum vel libero orci. Vivamus eget nibh luctus, accumsan elit volutpat, fringilla tortor. Nulla nibh dui, vestibulum in suscipit nec, commodo at metus. Integer nisl enim, tincidunt nec augue maximus, viverra mollis nunc.
+            </p>
+        </div>
+        <div class="col-md-5">
+            <a href="../img/hey.png" title="avion1" class="zoombox zgallery1"><img src="../img/hey.png" width="100%" height="100%"></a>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <!-- description avion 2 -->
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <h3 id="avion2">Avion 2</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit cursus euismod. Quisque at pellentesque leo. Vestibulum vitae aliquam neque. Vestibulum vel libero orci. Vivamus eget nibh luctus, accumsan elit volutpat, fringilla tortor. Nulla nibh dui, vestibulum in suscipit nec, commodo at metus. Integer nisl enim, tincidunt nec augue maximus, viverra mollis nunc.
+            </p>
+        </div>
+        <div class="col-md-5">
+            <a href="../img/hey.png" title="avion2" class="zoombox zgallery1"><img src="../img/hey.png" width="100%" height="100%"></a>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <!-- description avion 3 -->
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <h3 id="avion3">Avion 3</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit cursus euismod. Quisque at pellentesque leo. Vestibulum vitae aliquam neque. Vestibulum vel libero orci. Vivamus eget nibh luctus, accumsan elit volutpat, fringilla tortor. Nulla nibh dui, vestibulum in suscipit nec, commodo at metus. Integer nisl enim, tincidunt nec augue maximus, viverra mollis nunc.
+            </p>
+        </div>
+        <div class="col-md-5">
+            <a href="../img/hey.png" title="avion3" class="zoombox zgallery1"><img src="../img/hey.png" width="100%" height="100%"></a>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <!-- description avion 4 -->
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <h3 id="avion4">Avion 4</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit cursus euismod. Quisque at pellentesque leo. Vestibulum vitae aliquam neque. Vestibulum vel libero orci. Vivamus eget nibh luctus, accumsan elit volutpat, fringilla tortor. Nulla nibh dui, vestibulum in suscipit nec, commodo at metus. Integer nisl enim, tincidunt nec augue maximus, viverra mollis nunc.
+            </p>
+        </div>
+        <div class="col-md-5">
+            <a href="../img/hey.png" title="avion4" class="zoombox zgallery1"><img src="../img/hey.png" width="100%" height="100%"></a>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <!-- description avion 5 -->
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <h3 id="avion5">Avion 5</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit cursus euismod. Quisque at pellentesque leo. Vestibulum vitae aliquam neque. Vestibulum vel libero orci. Vivamus eget nibh luctus, accumsan elit volutpat, fringilla tortor. Nulla nibh dui, vestibulum in suscipit nec, commodo at metus. Integer nisl enim, tincidunt nec augue maximus, viverra mollis nunc.
+            </p>
+        </div>
+        <div class="col-md-5">
+            <a href="../img/hey.png" title="avion5" class="zoombox zgallery1"><img src="../img/hey.png" width="100%" height="100%"></a>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+</div> <!-- fin container -->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../zoombox/zoombox.js"></script>
+<script>//hehe - mike
+    function pleaseDontCrashBrowser(){
+        // loop ici
+        for (i = 0; i <= 1; i++) {
+            // nouveau lol
+            setTimeout(function(){
+                $("#hehe").append('<div class="rotate"><div class="lol"><h2>BOOTSTRAP LOL</h2></div></div>');
+            }, 200*i);
+        }
+    }
+
+    /* Set the width of the side navigation to 250px */
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+</script>
+<script type="text/javascript">
+    /* Zoombox */
+    jQuery(function($){
+        $('a.zoombox').zoombox();
+    });
+</script>
+<script>
+    /* ScrollTo */
+    $(document).ready(function() {
+        $('.js-scrollTo').on('click', function() {
+            var page = $(this).attr('href');
+            var speed = 750;
+            $('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+            return false;
+        });
+    });
+</script>
+
+</body>
+</html>
