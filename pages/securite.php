212c7d97191fa369dd52b7ba703d5bb846057fba
Michael <ac2031@hotmail.com>
2016-11-14 01:02:43 +0100

    grille, anim js

---
+++ pages/securite.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="Vintage Wings"/>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://www.vintagewings.com" />
+    <meta property="og:image" content="http://www.vintagewings.com/images/og.jpg" />
+    <meta name="description" content="Reservez un voyage dans un avion légéndaire!">
+    <meta name="author" content="Vintage Wings">
+    <meta name="keywords" content="Vintage, Wings, Vol, Reservation">
+    <link rel="icon" type="image/ico" href="http://195.83.128.55/~mmid116a05/img/icone1.ico">
+    <title>Vintage Wings</title>
+    <script src="https://use.fontawesome.com/ffa9c99182.js"></script>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/baseStyle.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        .jumbotron{
+            background-image: url("../img/bg44.jpg");
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-size: cover;
+            height: 350px;
+            width: 100%;
+            overflow: hidden;
+        }
+        .nav{
+            padding-left: 55px;
+        }
+        .navbar-right{
+            margin-right: 55px !important;
+        }
+        .compte{
+            margin-right: 95px!important;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 500;
+            color: #323232;
+        }
+        .blackened{
+            background-color: #fffaf4;
+            color: #323232;
+        }
+        .nav{
+            font-family: 'Raleway', sans-serif;
+            font-weight: 500;
+        }
+        .page-header{
+            border-bottom-color: #b9b9b9;
+        }
+        .panel, .securite-hover{
+            text-align: center;
+        }
+        .panel{
+            padding: 50px;
+            display: none;
+        }
+    </style>
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Taviraj" rel="stylesheet">
+</head>
+<body>
+
+    <?php include("navbar.php"); ?>
+
+    <div class="jumbotron">
+    </div>
+
+    <div class="container">
+        <div class="page-header">
+            <h1> Sécurité au vol, notre première priorité.</h1>
+        </div>
+    <div class="row">
+    <div class="col-md-12">
+    <div class="card-deck-wrapper margintop">
+        <div class="card-deck">
+        <div class="row">
+        <div class="col-sm-4">
+          <div class="card">
+            <a href="#">
+              <div class="securite-hover">
+                <div class="securite-hover-content">
+                  <i class="fa fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img src="../img/air.jpg" class="img-responsive" alt="">
+            </a>
+            <div class="card-block">
+              <h4 class="card-title">LES PLUS HAUTS STANDARDS</h4>
+              <p class="card-text"> Nous nous engageons à remplir toutes les règles internationales de sécurité au vol.</p>
+              <p class="card-text"><small class="text-muted">Voir plus</small></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <a href="#">
+              <div class="securite-hover">
+                <div class="securite-hover-content">
+                  <i class="fa fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img src="../img/air.jpg" class="img-responsive" alt="">
+            </a>
+            <div class="card-block">
+              <h4 class="card-title">LES PLUS HAUTS STANDARDS</h4>
+              <p class="card-text"> Nous nous engageons à remplir toutes les règles internationales de sécurité au vol.</p>
+              <p class="card-text"><small class="text-muted">Voir plus</small></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <a href="#">
+              <div class="securite-hover">
+                <div class="securite-hover-content">
+                  <i class="fa fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img src="../img/air.jpg" class="img-responsive" alt="">
+            </a>
+            <div class="card-block">
+              <h4 class="card-title">LES PLUS HAUTS STANDARDS</h4>
+              <p class="card-text"> Nous nous engageons à remplir toutes les règles internationales de sécurité au vol.</p>
+              <p class="card-text"><small class="text-muted">Voir plus</small></p>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+        <div class="card-deck margintop">
+        <div class="row">
+        <div class="col-sm-4">
+          <div class="card">
+            <a href="#">
+              <div class="securite-hover">
+                <div class="securite-hover-content">
+                  <i class="fa fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img src="../img/air.jpg" class="img-responsive" alt="">
+            </a>
+            <div class="card-block">
+              <h4 class="card-title">LES PLUS HAUTS STANDARDS</h4>
+              <p class="card-text"> Nous nous engageons à remplir toutes les règles internationales de sécurité au vol.</p>
+              <p class="card-text"><small class="text-muted">Voir plus</small></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <a href="#">
+              <div class="securite-hover">
+                <div class="securite-hover-content">
+                  <i class="fa fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img src="../img/air.jpg" class="img-responsive" alt="">
+            </a>
+            <div class="card-block">
+              <h4 class="card-title">LES PLUS HAUTS STANDARDS</h4>
+              <p class="card-text"> Nous nous engageons à remplir toutes les règles internationales de sécurité au vol.</p>
+              <p class="card-text"><small class="text-muted">Voir plus</small></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+              <div class="securite-hover">
+                <div class="securite-hover-content">
+                  <i class="fa fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img src="../img/air.jpg" class="img-responsive" alt="">
+            <div class="card-block">
+              <h4 class="card-title">LES PLUS HAUTS STANDARDS</h4>
+              <p class="card-text"> Nous nous engageons à remplir toutes les règles internationales de sécurité au vol.</p>
+              <p class="card-text"><small class="text-muted">Voir plus</small></p>
+            </div>
+            <div class="panel">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam felis ut condimentum facilisis. Phasellus nibh arcu, fringilla et turpis eget, lobortis dignissim ex. Maecenas pulvinar eros ipsum, quis dapibus velit feugiat a. Praesent molestie ut magna sed luctus. Sed maximus leo erat, ut laoreet augue rutrum ut. Proin id varius purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ornare leo bibendum erat hendrerit efficitur. Nullam varius orci hendrerit sodales aliquam. Maecenas et purus eros. Donec ultricies nisl non ex sagittis rutrum. Fusce in mollis magna.
+
+            Aliquam quis tellus vitae ipsum finibus laoreet quis nec leo. Etiam varius tristique ante ac semper. Vivamus a blandit diam, volutpat rutrum libero. Morbi rutrum, magna at rhoncus pretium, nibh mi dictum mi, quis tristique diam quam ac magna. Duis facilisis mauris ut ex tristique, pretium laoreet justo blandit. Proin commodo purus sit amet tortor volutpat pretium. Etiam blandit est vitae sem porta facilisis. Maecenas rhoncus non quam et sagittis.</div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+        </div>
+    </div>
+    </div>
+
+<?php include('footer.php'); ?>
+
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script>//hehe - mike
+    function pleaseDontCrashBrowser(){
+        // loop ici
+        for (i = 0; i <= 1; i++) {
+            // nouveau lol
+            setTimeout(function(){
+                $("#hehe").append('<div class="rotate"><div class="lol"><h2>BOOTSTRAP LOL</h2></div></div>');
+            }, 200*i);
+        }
+    }
+
+    /*PANIER: Set the width of the side navigation to 250px */
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    /*PANIER: Set the width of the side navigation to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+
+
+    /* SLIDE SECURITE */
+    $(document).ready(function(){
+    $(".securite-hover").click(function(){
+        $(".panel").slideToggle("slow");
+    });
+});
+</script>
+<?php include('footer.php'); ?>
+</body>
+</html>
