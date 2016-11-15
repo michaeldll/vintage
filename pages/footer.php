622df5e5b37ca5f2ca293021f9c74f783c02b494
Seyort <mathieu.gilles10@gmail.com>
2016-11-15 14:36:40 +0100

    css footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,63 +1,63 @@
 <?php echo '<footer>
-  <div class="container-fluid couleurBleu">
+  <div class="container-fluid fondCuirBleu">
     <div class="container">
       <div class="row marginTop10">
         <div class="col-md-3">
           <p class="padding10">
-            <a href="contact.php" class="whiteText">Nous contacter</a>
+            <a href="contact.php" class="whiteText footerLink">Nous contacter</a>
           </p>
         </div>
         <div class="col-md-3">
           <p class="padding10">
-            <a href="#" class="whiteText">Sécurité</a>
+            <a href="#" class="whiteText footerLink">Sécurité</a>
           </p>
         </div>
         <div class="col-md-3">
           <p class="padding10">
-            <a href="#" class="whiteText">A propos</a>
+            <a href="#" class="whiteText footerLink">A propos</a>
           </p>
         </div>
         <div class="col-md-3">
           <p class="padding10">
-            <a href="#" class="whiteText">Aéroport Paris - Le Bourget</a>
+            <a href="#" class="whiteText footerLink">Aéroport Paris - Le Bourget</a>
           </p>
         </div>
       </div>
       <div class="row">
         <div class="col-md-3">
           <p class="padding10">
-            <a href="#myModal" data-toggle="modal" class="whiteText">Mon Compte</a>
+            <a href="#myModal" data-toggle="modal" class="whiteText footerLink">Mon Compte</a>
           </p>
         </div>
         <div class="col-md-3">
           <p class="padding10">
-            <a href="boutique.php" class="whiteText">Boutique</a>
+            <a href="boutique.php" class="whiteText footerLink">Boutique</a>
           </p>
         </div>
         <div class="col-md-3">
           <p class="padding10">
-            <a href="#" class="whiteText">Wings Points</a>
+            <a href="#" class="whiteText footerLink">Wings Points</a>
           </p>
         </div>
         <div class="col-md-3">
           <p class="padding10">
-            <a href="#" class="whiteText">Informations vols</a>
+            <a href="#" class="whiteText footerLink">Informations vols</a>
           </p>
         </div>
       </div>
       <br/>
       <div class="row">
         <div class="col-md-4">
-          <p class="whiteText padding10">&copy; Copyrirht - Tous droits réservés</p>
+          <p class="whiteText padding10">&copy; Copyright - Tous droits réservés</p>
         </div>
         <div class="col-md-4">
           <div class="centered">
-            <p><a href="#" class="whiteText padding10">Informations légales</a></p>
+            <p><a href="#" class="whiteText padding10 footerLink">Informations légales</a></p>
           </div>
         </div>
         <div class="col-md-4">
           <div class="rightPosition">
-            <p class="whiteText padding10">Powered by <a href="http://195.83.128.55/~mmi15g11/projettut/intside/index.html" class="whiteText">Intside</a></p>
+            <p class="whiteText padding10">Powered by <a href="http://195.83.128.55/~mmi15g11/projettut/intside/index.html" class="whiteText footerLink">Intside</a></p>
           </div>
         </div>
       </div>
